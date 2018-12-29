d92b156e8a4b4943fe908ba7378ffc253b76f6b5
Ilya Zheil <ilya.zheil@gmail.com>
2018-12-29 18:39:34 +0200

    init

---
+++ libzheil/src/main/java/com/zheil/libzheil/ZhlExt.kt
@@ -0,0 +1,22 @@
+package com.zheil.libzheil
+
+import android.widget.EditText
+
+
+fun EditText.ifFocus(onHasFocus: () -> Unit) {
+    this.setOnFocusChangeListener { _, hasFocus ->
+        if (hasFocus) {
+            onHasFocus()
+        }
+    }
+}
+
+fun EditText.ifFocusElse(onHasFocus: () -> Unit, onNotFocus: () -> Unit) {
+    this.setOnFocusChangeListener { _, hasFocus ->
+        if (hasFocus) {
+            onHasFocus()
+        } else {
+            onNotFocus()
+        }
+    }
+}
